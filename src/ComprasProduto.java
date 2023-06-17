6b33cc44fc0b36eba8f5bf941eff5799142e078a
João <joaoaugusto311016@gmail.com>
2023-06-17 18:22:47 -0300

    17-06

--- src/ComprasProduto.java
+++ src/ComprasProduto.java
@@ -2,4 +2,21 @@ public class ComprasProduto {
     private double valorProduto;
     private int QuantidadePedidos;
     private String descricaoProduto;
+
+    public ComprasProduto(double valorProduto, String descricaoProduto) {
+        this.valorProduto = valorProduto;
+        this.descricaoProduto = descricaoProduto;
+    }
+
+    public double getValorProduto() {
+        return valorProduto;
+    }
+
+    public int getQuantidadePedidos() {
+        return QuantidadePedidos;
+    }
+
+    public String getDescricaoProduto() {
+        return descricaoProduto;
+    }
 }
